56e712accf4bebc9b395f6d2891c90a34041c093
vilhelmmelkstam <vilhelm.melkstam@gmail.com>
2019-10-22 20:14:03 +0200

    use css smooth scroll behaviour instead of js

--- src/components/navbarComponent.js
+++ src/components/navbarComponent.js
@@ -10,7 +10,7 @@ const NavbarComponent = () => (
       </Navbar.Brand>
       <Navbar.Menu active="true">
         <Navbar.Segment align="end">
-          <Navbar.Item onClick={() => scrollTo('about-me-section')}>About me</Navbar.Item>
+          <Navbar.Item href="#about-me-section">About me</Navbar.Item>
           <Navbar.Item href="#">Projects</Navbar.Item>
           <Navbar.Item href="#">Contact</Navbar.Item>
         </Navbar.Segment>
@@ -18,11 +18,5 @@ const NavbarComponent = () => (
     </Container>
   </Navbar>
 );
-
-function scrollTo(id) {
-  document.getElementById(id).scrollIntoView({
-    behavior: 'smooth',
-  });
-}
 
 export default NavbarComponent;
