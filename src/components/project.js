933cf1bee3a7f34de8c8ca2af5f6c8fd1db76268
vilhelmmelkstam <vilhelm.melkstam@gmail.com>
2019-10-22 20:34:04 +0200

    update repository in package.json

--- src/components/project.js
+++ src/components/project.js
@@ -8,38 +8,38 @@ import Image from './image';
 const Project = ({
   id, title, image, description, readMoreLink,
 }) => (
-    <Column
-      mobile={{ size: 'half' }}
-      tablet={{ size: 'half' }}
-      desktop={{ size: 'one-third' }}
-      widescreen={{ size: 'one-quarter' }}
-      fullhd={{ size: 'one-quarter' }}
-      key={id}
-    >
-      <Card className="project-card">
-        <Card.Image>
-          <div>
-            <Image alt={title} filename={image} />
-          </div>
-        </Card.Image>
-        <Card.Header>
-          <Card.Header.Title>
-            {title}
-          </Card.Header.Title>
-        </Card.Header>
-        <Card.Content>
-          <p>{description}</p>
-        </Card.Content>
-        <Card.Footer>
-          <a href={readMoreLink}>
-            <Card.Footer.Item>
+  <Column
+    mobile={{ size: 'half' }}
+    tablet={{ size: 'half' }}
+    desktop={{ size: 'one-third' }}
+    widescreen={{ size: 'one-quarter' }}
+    fullhd={{ size: 'one-quarter' }}
+    key={id}
+  >
+    <Card className="project-card">
+      <Card.Image>
+        <div>
+          <Image alt={title} filename={image} />
+        </div>
+      </Card.Image>
+      <Card.Header>
+        <Card.Header.Title>
+          {title}
+        </Card.Header.Title>
+      </Card.Header>
+      <Card.Content>
+        <p>{description}</p>
+      </Card.Content>
+      <Card.Footer>
+        <a href={readMoreLink}>
+          <Card.Footer.Item>
               Read more
-            </Card.Footer.Item>
-          </a>
-        </Card.Footer>
-      </Card>
-    </Column>
-  );
+          </Card.Footer.Item>
+        </a>
+      </Card.Footer>
+    </Card>
+  </Column>
+);
 
 Project.propTypes = {
   id: PropTypes.string.isRequired,
