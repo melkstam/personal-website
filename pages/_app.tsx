efdf246ebb6046f4d7181d539451ff3910cf863b
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2022-05-06 12:05:01 +0200

    feat: add seo and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,19 @@
 import React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
 import '@fontsource/fira-code';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <DefaultSeo
+      titleTemplate="%s | Vilhelm Melkstam"
+      defaultTitle="Vilhelm Melkstam"
+      description="Fullstack developer and nerd, here I am."
+    />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
