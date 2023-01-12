5af5dda17895186e4dbae8824242a0ed8bb1b8ad
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2023-01-12 16:00:55 +0100

    better size handling of cover image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import headerImage from "../public/img/oily-bw.jpg";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../tailwind.config.js";
+
+const twConfig = resolveConfig(tailwindConfig);
 
 import { allPosts } from "contentlayer/generated";
 
@@ -12,6 +16,8 @@ const dateFormatter = Intl.DateTimeFormat("sv-SE", {
   dateStyle: "long",
 });
 
+const screens = twConfig.theme?.screens as Record<string, string>;
+
 function Home() {
   return (
     <main className="container mx-auto px-2 max-w-lg ">
@@ -19,12 +25,16 @@ function Home() {
         Melkstams{"\n"}Blogg
       </h1>
 
-      <Image
-        src={headerImage}
-        alt="Header image"
-        priority
-        className="h-40 object-cover mt-8"
-      />
+      <div className="mt-8 relative h-40">
+        <Image
+          src={headerImage}
+          alt="Header image"
+          priority
+          fill
+          className="object-cover"
+          sizes={`(max-width: ${screens["lg"]}) 100vw, ${screens["lg"]}`}
+        />
+      </div>
 
       <h2 className="text-white/95 font-black text-4xl mt-12">Inlägg</h2>
 
